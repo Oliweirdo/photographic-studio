feecd63b0064771b281254610035fcc300c82697
Oliwia <Oliweirdo@gmail.com>
2022-08-05 11:36:32 +0200

    fixed styles and password issue

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import '../scss/main.scss';
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const image=document.querySelector("#protect");
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active");
@@ -14,7 +15,8 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         navMenu.classList.remove("active");
     }))
 
-    document.getElementById('protect').onClick = function run() {
+
+    image.addEventListener("click",()=>{
         var testV = 1;
         var pass1 = prompt('Strona dostępna jest wyłącznie dla klientów. Proszę podaj hasło by przejść dalej.',' ');
         while (testV < 2) {
@@ -27,6 +29,5 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         }
         if (pass1.toLowerCase()!="password" & testV ==3) history.go(-1);
         return " ";
-    }
-    
+    } )
     
